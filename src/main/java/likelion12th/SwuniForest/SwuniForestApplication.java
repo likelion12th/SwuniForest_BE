05c0279193f6121b452f36b1296f93823d13fbe3
yushinc <cys74351@gmail.com>
2024-02-22 16:04:49 +0900

    first commit

---
+++ src/main/java/likelion12th/SwuniForest/SwuniForestApplication.java
@@ -0,0 +1,13 @@
+package likelion12th.SwuniForest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwuniForestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwuniForestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/likelion12th/SwuniForest/SwuniForestApplicationTests.java
@@ -0,0 +1,13 @@
+package likelion12th.SwuniForest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwuniForestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
