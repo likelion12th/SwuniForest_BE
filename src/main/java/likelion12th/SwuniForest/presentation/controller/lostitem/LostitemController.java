c06e3f177294e5be1f492736ed58553963e071c3
Choi MinJung <mj020503@naver.com>
2024-05-06 18:07:18 +0900

    lostitem 패키지 설정

---
+++ src/main/java/likelion12th/SwuniForest/presentation/controller/lostitem/LostitemController.java
@@ -0,0 +1,11 @@
+package likelion12th.SwuniForest.presentation.controller.lostitem;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/lostitem")
+public class LostitemController {
+}
---
+++ src/main/java/likelion12th/SwuniForest/service/lostitem/LostitemService.java
@@ -0,0 +1,9 @@
+package likelion12th.SwuniForest.service.lostitem;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LostitemService {
+}
