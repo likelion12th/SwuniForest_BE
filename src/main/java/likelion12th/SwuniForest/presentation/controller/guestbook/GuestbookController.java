d759cc04d9a0bbcbb59b2c792b719ab1705f3163
Choi MinJung <mj020503@naver.com>
2024-05-06 17:04:09 +0900

    feat: 방명록 작성 API 구현(S3 이미지 업로드)

---
+++ src/main/java/likelion12th/SwuniForest/presentation/controller/guestbook/GuestbookController.java
@@ -0,0 +1,49 @@
+package likelion12th.SwuniForest.presentation.controller.guestbook;
+
+import likelion12th.SwuniForest.service.guestbook.GuestbookService;
+import likelion12th.SwuniForest.service.guestbook.S3Service;
+import likelion12th.SwuniForest.service.guestbook.domain.dto.GuestbookDto;
+import likelion12th.SwuniForest.service.guestbook.repository.GuestbookRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/guestbook")
+public class GuestbookController {
+
+    private final GuestbookRepository guestbookRepository;
+    private final GuestbookService guestbookService;
+    private final S3Service s3Service;
+
+    // 방명록 작성
+    @PostMapping("/post")
+    public ResponseEntity<String> postGuestbook(
+            @RequestPart(value = "guestbookDto") GuestbookDto guestbookDto,
+            @RequestPart(value = "imageFile", required = false) MultipartFile imageFile) {
+
+        String fileName = "";
+        if (imageFile != null && !imageFile.isEmpty()) {
+            // 이미지 업로드
+            try {
+                // S3 버킷의 guestbook 디렉토리 안에 저장됨
+                fileName = s3Service.upload(imageFile, "guestbook");
+            } catch (IOException e) {
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("이미지 업로드 중 오류가 발생했습니다.");
+            }
+        }
+
+        // 방명록 등록
+        try {
+            GuestbookDto response = guestbookService.createGuestbook(guestbookDto, fileName);
+            return ResponseEntity.ok("방명록이 성공적으로 작성되었습니다.");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("방명록 작성 중 오류가 발생했습니다.");
+        }
+    }
+}
--- src/main/java/likelion12th/SwuniForest/service/guestbook/domain/Guestbook.java
+++ src/main/java/likelion12th/SwuniForest/service/guestbook/domain/Guestbook.java
@@ -1,4 +1,34 @@
 package likelion12th.SwuniForest.service.guestbook.domain;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Guestbook {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="guestbook_id")
+    private Long id;
+
+    // 방명록 작성자 이름
+    private String guestName;
+
+    // 방명록 내용
+    @Column(nullable = false, length=50)
+    private String guestContent;
+
+    // 방명록 이미지
+    private String fileName;
+
+    // 익명 여부
+    @Column(nullable = false)
+    private boolean anonymous;
 }
