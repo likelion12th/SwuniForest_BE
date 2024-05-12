9947ff9d04f97fa9a8682d3098816475b3a3f8f8
yushinc <cys74351@gmail.com>
2024-05-13 00:37:41 +0900

    feat: 방문율 데이터 조회, 방문율 랭킹 전체 조회 api 추가

---
+++ src/main/java/likelion12th/SwuniForest/service/visit/domain/dto/RankingDto.java
@@ -0,0 +1,16 @@
+package likelion12th.SwuniForest.service.visit.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class RankingDto {
+    private String major;
+    private Long visitRate;
+    private Integer rank;
+}
--- src/main/java/likelion12th/SwuniForest/service/visit/repository/VisitRepository.java
+++ src/main/java/likelion12th/SwuniForest/service/visit/repository/VisitRepository.java
@@ -3,8 +3,11 @@ package likelion12th.SwuniForest.service.visit.repository;
 import likelion12th.SwuniForest.service.visit.domain.Visit;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface VisitRepository extends JpaRepository<Visit, Long> {
     Optional<Visit> findByMajor(String major);
+
+    List<Visit> findAllByOrderByVisitRateDesc();
 }
