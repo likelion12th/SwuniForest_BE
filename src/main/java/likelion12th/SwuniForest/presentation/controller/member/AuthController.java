682f715ace7968bf36c3cb713ef0de6a2dd71820
yushinc <cys74351@gmail.com>
2024-05-22 00:27:14 +0900

    fix: 로그인 에러 핸들링 상태코드 분리

--- src/main/java/likelion12th/SwuniForest/presentation/controller/member/AuthController.java
+++ src/main/java/likelion12th/SwuniForest/presentation/controller/member/AuthController.java
@@ -2,19 +2,14 @@ package likelion12th.SwuniForest.presentation.controller.member;
 
 
 import jakarta.validation.Valid;
-import likelion12th.SwuniForest.jwt.JwtFilter;
-import likelion12th.SwuniForest.jwt.TokenProvider;
+
 import likelion12th.SwuniForest.service.member.AuthService;
 import likelion12th.SwuniForest.service.member.domain.dto.LoginDto;
 import likelion12th.SwuniForest.service.member.domain.dto.TokenDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,16 +21,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     private final AuthService authService;
-    // 로그인 요청 시 jwt 토큰 발급
+
     @PostMapping("/login")
     public ResponseEntity<TokenDto> authorize(@Valid @RequestBody LoginDto loginDto) {
-        try {
-            // tokenDto를 이용해 response body에도 넣어서 리턴
-            TokenDto tokenDto = authService.login(loginDto);
-            return new ResponseEntity<>(tokenDto, HttpStatus.OK);
-        } catch (Exception e) {
-            return new ResponseEntity(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
-
-        }
+        TokenDto tokenDto = authService.login(loginDto);
+        return new ResponseEntity<>(tokenDto, HttpStatus.OK);
     }
 }
